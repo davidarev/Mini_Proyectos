759a2726856fce5ffc4c13e3f598f12e5547eb2a
David Arévalo <72552800+davidarev@users.noreply.github.com>
2022-11-28 19:18:12 +0100

    Update what_is_the_number.py

--- what_is_the_number.py
+++ what_is_the_number.py
@@ -1,12 +1,3 @@
-"""
-Hacer un programa donde la computadora genere un número aleatorio entre 1 y 100, y el usuario debe adivinarlo.
-Si el usuario falla, la computadora le debe decir si el número que debe adivinar es mayor o menor que el que ingresó el usuario.
-El programa debe terminar cuando el usuario adivine el número.
-Hay que tener en cuenta que el usuario puede ingresar cualquier cosa, por lo que hay que verificar que lo que ingresó sea un número válido.
-El programa debe mostrar un mensaje de error si el usuario ingresa algo que no sea un número en cada intento.
-Hacer uso de funciones.
-"""
-
 import random
 
 def is_valid_number(number): #Verifica que el número sea válido
